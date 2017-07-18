5801fe8892063845587ab2b77190f15d20fafd77
bintothemax <bin.mei@comcast.com>
2017-07-18 15:15:12 -0700

    Updated Spacing and Wrapped in ready.
    What summary said.

--- js/menu_toggle.js
+++ js/menu_toggle.js
@@ -1,47 +1,51 @@
-function showAll(){
-    $(".github-content li").show()
-}
+(function ($) {
+    $(function () {
+        function showAll(){
+            $(".github-content li").show()
+        }
 
-function hideAll(){
-    $(".github-content li").hide()
-}
+        function hideAll(){
+            $(".github-content li").hide()
+        }
 
-function showBackend(){
-    $(".github-content li").hide()
-	$(".github-content li.category-backend").show();
-}
+        function showBackend(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-backend").show();
+        }
 
-function showBigdata(){
-    $(".github-content li").hide()
-	$(".github-content li.category-bigdata").show();
-}	
+        function showBigdata(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-bigdata").show();
+        }	
 
-function showFrontend(){
-    $(".github-content li").hide()
-	$(".github-content li.category-frontend").show();
-}
+        function showFrontend(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-frontend").show();
+        }
 
-function showHardware(){
-    $(".github-content li").hide()
-	$(".github-content lt.category-hardware").show();
-}	
+        function showHardware(){
+            $(".github-content li").hide()
+	    $(".github-content lt.category-hardware").show();
+        }	
 
-function showInfra(){
-    $(".github-content li").hide()
-	$(".github-content li.category-infra").show();
-}
+        function showInfra(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-infra").show();
+        }
 
-function showMobile(){
-    $(".github-content li").hide()
-	$(".github-content li.category-mobile").show();
-}
+        function showMobile(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-mobile").show();
+        }
 
-function showTesting(){
-    $(".github-content li").hide()
-	$(".github-content li.category-testing").show();
-}
+        function showTesting(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-testing").show();
+        }
 
-function showVideo(){
-    $(".github-content li").hide()
-	$(".github-content li.category-video").show();
-}
+        function showVideo(){
+            $(".github-content li").hide()
+	    $(".github-content li.category-video").show();
+	}
+    });
+}(jQuery));
